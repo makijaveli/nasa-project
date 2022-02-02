502faaf98faacb19845b0324e3842db3b643a416
Milos Avramovic <avramovic.m@oilpc.ru>
2022-02-02 14:02:49 +0100

    Async call for fetch planets data.

--- server/src/server.js
+++ server/src/server.js
@@ -5,6 +5,16 @@ const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-    console.log("Listening on port " + PORT);
-});
+const {loadPlanetData} = require('./models/planets.model')
+
+// Start server
+async function startServer() {
+    await loadPlanetData();
+    // Start server
+    server.listen(PORT, () => {
+        console.log("Listening on port " + PORT);
+    });
+}
+
+startServer();
+
